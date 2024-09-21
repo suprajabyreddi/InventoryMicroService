1a20cd3945dc06cf52ada36254d903a63f0d57ed
pujit <bsupraja7891@gmail.com>
2024-09-20 21:40:19 -0400

    application

---
+++ src/main/java/com/demo/InventoryMicroService/InventoryMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.InventoryMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMicroServiceApplication.class, args);
+	}
+
+}
